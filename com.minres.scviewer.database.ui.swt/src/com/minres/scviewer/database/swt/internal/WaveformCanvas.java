23131e7527497ed4f1487499487d0c6728c556a1
Brita Keller <brita@minres.com>
2018-09-10 20:39:20 +0200

    Fixed ZoomFit

--- com.minres.scviewer.database.ui.swt/src/com/minres/scviewer/database/swt/internal/WaveformCanvas.java
+++ com.minres.scviewer.database.ui.swt/src/com/minres/scviewer/database/swt/internal/WaveformCanvas.java
@@ -56,7 +56,7 @@ public class WaveformCanvas extends Canvas {
     
     private int level = 12;
     
-    public final static String[] unitString={"fs", "ps", "ns", "Âµs", "ms"};//, "s"};
+    public final static String[] unitString={"fs", "ps", "ns", "µs", "ms"};//, "s"};
     
     public final static int[] unitMultiplier={1, 3, 10, 30, 100, 300};
     
