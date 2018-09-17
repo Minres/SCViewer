021c2bceff2a14bc621c305971dcc4a57641de82
Brita Keller <brita@minres.com>
2018-09-17 17:06:57 +0200

    Fixed ZoomFit and encoding issue

--- com.minres.scviewer.database.ui.swt/src/com/minres/scviewer/database/swt/internal/WaveformCanvas.java
+++ com.minres.scviewer.database.ui.swt/src/com/minres/scviewer/database/swt/internal/WaveformCanvas.java
@@ -56,7 +56,7 @@ public class WaveformCanvas extends Canvas {
     
     private int level = 12;
     
-    public final static String[] unitString={"fs", "ps", "ns", "µs", "ms"};//, "s"};
+    public final static String[] unitString={"fs", "ps", "ns", "us", "ms"};//, "s"};
     
     public final static int[] unitMultiplier={1, 3, 10, 30, 100, 300};
     
