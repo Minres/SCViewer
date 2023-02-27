c45d7b502464487052ebcd0422516bcdca80db0d
Eyck Jentzsch <eyck@minres.com>
2023-02-27 18:34:27 +0100

    add windows fst library

--- plugins/com.minres.scviewer.database.fst/csrc/fst/build.sh
+++ plugins/com.minres.scviewer.database.fst/csrc/fst/build.sh
@@ -1,2 +1,2 @@
-cmake -B build -S . -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../../linux-x86-64
+cmake -B build -S . -DCMAKE_INSTALL_PREFIX=../../linux-x86-64 -DCMAKE_BUILD_TYPE=Release
 cmake --build build --target install
